4386f3791f0a28741a7977076f39b6788cfe65b0
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 20:17:30 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 /* eslint-disable immutable/no-let, no-magic-numbers, no-eval, flowtype/require-variable-type */
-
-export default function streamSatisfies (example: string | $ReadOnlyArray<mixed>): UnaryFunctionType {
-  return function streamSatisfiesExample (assertion: mixed => mixed => boolean): UnaryFunctionType {
-    return function streamSatisfiesExampleAssertion (failure: mixed => mixed): UnaryFunctionType {
-      return function streamSatisfiesExampleFailureAssertion (finishing: string => number => mixed): UnaryFunctionType {
-        return function streamSatisfiesExampleFailureAssertionFinishing (stream: StreamType): UnaryFunctionType {
-          const marbles = example instanceof Array ? example : example.split("---")
-          const expectation = example instanceof Array ? marbles : Reflect.apply(Array.prototype.slice, marbles, [0, -1]).map((source: string): mixed => eval(`(${source})`))
-          const isCompletable = example instanceof Array ? true : marbles[marbles.length - 1] === "|"
-          let position = 0
-
+export default function streamSatisfies(example) {
+  return function streamSatisfiesExample(assertion) {
+    return function streamSatisfiesExampleAssertion(failure) {
+      return function streamSatisfiesExampleFailureAssertion(finishing) {
+        return function streamSatisfiesExampleFailureAssertionFinishing(stream) {
+          const marbles = example instanceof Array ? example : example.split("---");
+          const expectation = example instanceof Array ? marbles : Reflect.apply(Array.prototype.slice, marbles, [0, -1]).map(source => eval(`(${source})`));
+          const isCompletable = example instanceof Array ? true : marbles[marbles.length - 1] === "|";
+          let position = 0;
           return stream.subscribe({
-            next (value: mixed) {
-              assertion(value)(expectation[position])
+            next(value) {
+              assertion(value)(expectation[position]);
+              position += 1;
+            },
 
-              position += 1
+            error(exception) {
+              failure(exception);
             },
-            error (exception: Error) {
-              failure(exception)
-            },
-            complete () {
+
+            complete() {
               if (isCompletable) {
-                finishing(expectation)(position)
+                finishing(expectation)(position);
               } else {
-                finishing(expectation)(position)
+                finishing(expectation)(position);
               }
-            },
-          })
-        }
-      }
-    }
-  }
+            }
+
+          });
+        };
+      };
+    };
+  };
 }
