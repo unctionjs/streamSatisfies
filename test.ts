d424f2b8327268d0a4ab321adad2ae132e6bfdfd
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-09-05 22:58:53 -0700

    Moving to jest and typing the test file

--- test.ts
+++ test.ts
@@ -1,152 +1,151 @@
 /* eslint-disable no-magic-numbers, flowtype/require-variable-type */
-import {test} from "tap";
-import {of} from "most";
+import { of } from "most";
 import {from} from "most";
 import {merge} from "most";
 
-import streamSatisfies from "./";
+import streamSatisfies from "./index.ts";
 
-test("String diagram", ({equal, doesNotThrow, end}) => {
+test("String diagram", done => {
   const left = of("a");
   const right = of("b");
 
   streamSatisfies(
     "'b'---'a'---|"
   )(
-    (given) => (expected) => equal(given, expected)
+    (given) => (expected) => expect(given).toBe(expected)
   )(
     doesNotThrow
   )(
     ({length}) => (size) => {
-      equal(length, size);
+      expect(length).toBe(size);
 
-      end();
+      done();
     }
   )(
     merge(right, left)
   );
 });
 
-test("String diagram", ({equal, doesNotThrow, end}) => {
+test("String diagram", done => {
   const left = of(1);
   const right = of(2);
 
   streamSatisfies(
     "2---1---|"
   )(
-    (given) => (expected) => equal(given, expected)
+    (given) => (expected) => expect(given).toBe(expected)
   )(
     doesNotThrow
   )(
     ({length}) => (size) => {
-      equal(length, size);
+      expect(length).toBe(size);
 
-      end();
+      done();
     }
   )(
     merge(right, left)
   );
 });
 
-test("String diagram", ({same, equal, doesNotThrow, end}) => {
+test("String diagram", done => {
   const left = of({aaa: "aaa"});
   const right = of({bbb: "bbb"});
 
   streamSatisfies(
     "{bbb: \"bbb\"}---{aaa: \"aaa\"}---|"
   )(
-    (given) => (expected) => same(given, expected)
+    (given) => (expected) => expect(given).toEqual(expected)
   )(
     doesNotThrow
   )(
     ({length}) => (size) => {
-      equal(length, size);
+      expect(length).toBe(size);
 
-      end();
+      done();
     }
   )(
     merge(right, left)
   );
 });
 
-test("String diagram", ({equal, doesNotThrow, end}) => {
+test("String diagram", done => {
   const left = of("a");
   const right = of("b");
 
   streamSatisfies(
     "'b'---'a'--->"
   )(
-    (given) => (expected) => equal(given, expected)
+    (given) => (expected) => expect(given).toBe(expected)
   )(
     doesNotThrow
   )(
     ({length}) => (size) => {
-      equal(length, size);
+      expect(length).toBe(size);
 
-      end();
+      done();
     }
   )(
     merge(right, left)
   );
 });
 
-test("Array diagram", ({equal, doesNotThrow, end}) => {
+test("Array diagram", done => {
   const left = of("a");
   const right = of("b");
 
   streamSatisfies(
     ["b", "a"]
   )(
-    (given) => (expected) => equal(given, expected)
+    (given) => (expected) => expect(given).toBe(expected)
   )(
     doesNotThrow
   )(
     ({length}) => (size) => {
-      equal(length, size);
+      expect(length).toBe(size);
 
-      end();
+      done();
     }
   )(
     merge(right, left)
   );
 });
 
-test("Array diagram", ({equal, doesNotThrow, end}) => {
+test("Array diagram", done => {
   const left = of(1);
   const right = of(2);
 
   streamSatisfies(
     [2, 1]
   )(
-    (given) => (expected) => equal(given, expected)
+    (given) => (expected) => expect(given).toBe(expected)
   )(
     doesNotThrow
   )(
     ({length}) => (size) => {
-      equal(length, size);
+      expect(length).toBe(size);
 
-      end();
+      done();
     }
   )(
     merge(right, left)
   );
 });
 
-test("Array diagram", ({same, equal, doesNotThrow, end}) => {
+test("Array diagram", done => {
   const left = of({aaa: "aaa"});
   const right = of({bbb: "bbb"});
 
   streamSatisfies(
     [{bbb: "bbb"}, {aaa: "aaa"}]
   )(
-    (given) => (expected) => same(given, expected)
+    (given) => (expected) => expect(given).toEqual(expected)
   )(
     doesNotThrow
   )(
     ({length}) => (size) => {
-      equal(length, size);
+      expect(length).toBe(size);
 
-      end();
+      done();
     }
   )(
     merge(right, left)
@@ -154,7 +153,7 @@ test("Array diagram", ({same, equal, doesNotThrow, end}) => {
 });
 
 
-test("Array diagram with error", ({equal, match, end}) => {
+test("Array diagram with error", done => {
   const stream = from([
     {unction: () => true},
     {unction: () => true},
@@ -165,17 +164,17 @@ test("Array diagram with error", ({equal, match, end}) => {
   streamSatisfies(
     [true, true]
   )(
-    (given) => (expected) => equal(given, expected)
+    (given) => (expected) => expect(given).toBe(expected)
   )(
     (exception) => {
-      match(exception, TypeError);
-      end();
+      t.match(exception, TypeError);
+      done();
     }
   )(
     ({length}) => (size) => {
-      equal(length, size);
+      expect(length).toBe(size);
 
-      end();
+      done();
     }
   )(
     stream
